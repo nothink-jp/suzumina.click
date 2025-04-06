2ae0e0cc063151f74507539f7e321fe94fa841cc
nothink <gakongakon@gmail.com>
2025-04-07 08:53:13 +0900

    fix(terraform): update ignore_changes path based on error message
    Update the path in lifecycle.ignore_changes for the Cloud Run image to 'template[0].containers[0].image' as suggested by the Terraform error message, although standard syntax suggests otherwise.

--- iac/environments/dev/main.tf
+++ iac/environments/dev/main.tf
@@ -131,8 +131,8 @@ resource "google_cloud_run_v2_service" "web" {
   # CI/CD でイメージが更新されるため、イメージの変更は Terraform で無視する
   lifecycle {
     ignore_changes = [
-      # template ブロック内の containers リストの最初の要素の image 属性を指定
-      template.containers[0].image,
+      # エラーメッセージに従い、インデックスを使用
+      template[0].containers[0].image,
     ]
   }
 
