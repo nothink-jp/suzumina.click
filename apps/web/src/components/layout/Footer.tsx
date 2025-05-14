24b3201c8a1b19d353e112e694e0b26af10a65e0
nothink <gakongakon@gmail.com>
2025-05-14 09:13:13 +0900

    fix: footer update

--- apps/web/src/components/layout/Footer.tsx
+++ apps/web/src/components/layout/Footer.tsx
@@ -9,9 +9,10 @@ export default function Footer() {
       {/* DaisyUI Footer */}
       <aside>
         <p>
-          Copyright © {currentYear} - All right reserved by nothink.jp
+          Copyright © {currentYear} - Some right reserved by{" "}
+          <Link href="https://www.nothink.jp">nothink.jp</Link>
           <br />
-          このサイトは涼花みなせ様の非公式ファンサイトであり、ご本人様及び関係者様とは一切関係ありません。
+          このサイトは非公式な情報サイトであり、涼花みなせ様ご本人様及び関係者様とは一切関係ありません。
         </p>
         {/* 必要に応じて他のリンクや情報を追加 */}
         {/* 例: <Link href="/privacy-policy" className="link link-hover">プライバシーポリシー</Link> */}
