915c703dbcbcbec4628151cf30abe3968a60c41f
nothink <gakongakon@gmail.com>
2025-07-28 10:21:53 +0900

    fix: layoutモジュールからの不要なexportを削除

--- apps/web/src/components/layout/index.ts
+++ apps/web/src/components/layout/index.ts
@@ -1,4 +1,3 @@
-export { HomePage } from "./home-page";
 export { default as MobileMenu } from "./mobile-menu";
 export { default as SiteFooter } from "./site-footer";
 export { default as SiteHeader } from "./site-header";
